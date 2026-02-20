ac1710b1adb4af79f0b80b0fa63240434255bc98
Matheus-H-S-Amorim <matheushsa@al.insper.br>
2026-02-20 11:41:54 -0300

    ex5 tentativa 2 nv

--- exe5/main.c
+++ exe5/main.c
@@ -25,16 +25,23 @@ int main() {
             printf("Botao 1: %d\n", cnt_1++);
 
             while(!gpio_get(BTN_PIN)){
-                sleep_ms(1000);
+                sleep_ms(10);
 
             };
+
+            sleep_ms(50);
         }
 
+        
         if (!gpio_get(BTN_PIN_2)) {
             printf("Botao 2: %d\n", cnt_2++);
             while(!gpio_get(BTN_PIN_2)){
-                sleep_ms(1000);
+                sleep_ms(10);
             };
+            
+            sleep_ms(50);
+
+            
         }
     }
 }
