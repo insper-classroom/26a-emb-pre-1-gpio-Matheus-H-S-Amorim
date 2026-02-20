36e27e99d04834932494c6023a0aef7635315136
Matheus-H-S-Amorim <matheushsa@al.insper.br>
2026-02-20 10:08:46 -0300

    usando p como ponteiro

--- exe0_variables/main.c
+++ exe0_variables/main.c
@@ -1,16 +1,18 @@
 #include "stdio.h"
 
 
-void foo(int *a) {
-    *a = *a + 1; // acessa variavel global
+void foo(int *p) {
+    *p = *p + 1; // acessa variavel global
 }
 
 void main(void) {
     int a = 0; // var global
     int b = 0; // var global
+    int *p;
+    p=&a;
 
     while (1) {
-        foo(&a);
+        foo(p);
         
         if (a > 5) {
             b = 1;
